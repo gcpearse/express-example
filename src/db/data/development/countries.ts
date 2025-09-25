06031d78d6d3ed323ad9d8f892b2bd41285a6621
gcpearse <glen.c.pearse@gmail.com>
2025-09-25 20:02:20 +0100

    Adds dev data example

---
+++ src/db/data/development/countries.ts
@@ -0,0 +1,16 @@
+import type { countryData } from "../../../types/data.js";
+
+export const countriesData: countryData[] = [
+  {
+    name: "France",
+    capital: "Paris",
+  },
+  {
+    name: "Italy",
+    capital: "Rome",
+  },
+  {
+    name: "Spain",
+    capital: "Madrid",
+  },
+]
