15fcfddd0b4ceae09c4443bdba28b6c2c72ecfb8
gcpearse <glen.c.pearse@gmail.com>
2025-09-25 22:52:06 +0100

    Adds error-handling middleware

---
+++ src/middleware/errors.ts
@@ -0,0 +1,54 @@
+import { ErrorRequestHandler, RequestHandler } from "express"
+
+const CODE_INVALID_TEXT_REPRESENTATION = "22P02"
+const CODE_NOT_NULL_VIOLATION = "23502"
+
+export const customErrorHandler: ErrorRequestHandler = (err, _req, res, next) => {
+
+  if (err.status) {
+    res.status(err.status).send({
+      message: err.message,
+      details: err.details
+    })
+  } else {
+    next(err)
+  }
+}
+
+export const psqlErrorHandler: ErrorRequestHandler = (err, _req, res, next) => {
+
+  if (err.code === CODE_INVALID_TEXT_REPRESENTATION) {
+    res.status(400).send({
+      message: "Bad Request",
+      details: "Invalid text representation"
+    })
+  } else if (err.code === CODE_NOT_NULL_VIOLATION) {
+    res.status(400).send({
+      message: "Bad Request",
+      details: "Not null violation"
+    })
+  } else {
+    next(err)
+  }
+}
+
+export const serverErrorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+
+  console.log(err.code)
+
+  res.status(500).send({
+    message: "Internal Server Error"
+  })
+}
+
+export const notFoundHandler: RequestHandler = async (_req, res, next) => {
+
+  try {
+    res.status(404).send({ 
+      message: "Not Found",
+      details: "Path not found" 
+    })
+  } catch (err) {
+    next(err)
+  }
+}
