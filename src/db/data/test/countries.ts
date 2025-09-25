2393f612998424fb2a291638502f3ded78effdc3
gcpearse <glen.c.pearse@gmail.com>
2025-09-25 19:32:51 +0100

    Adds raw test data

---
+++ src/db/data/test/countries.ts
@@ -0,0 +1,19 @@
+type countryData = {
+  name: string
+  capital: string
+}
+
+export const countriesData: countryData[] = [
+  {
+    name: "France",
+    capital: "Paris",
+  },
+  {
+    name: "Italy",
+    capital: "Rome",
+  },
+  {
+    name: "Spain",
+    capital: "Madrid",
+  },
+]
